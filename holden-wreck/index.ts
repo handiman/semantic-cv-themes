0f5c79e4117d31860bb6c947ac8999a8eb0807bb
handiman <handiman@users.noreply.github.com>
2026-05-14 17:52:36 +0200

    fix(Themes): Give Holden Wreck its own css for now

--- holden-wreck/index.ts
+++ holden-wreck/index.ts
@@ -34,24 +34,4 @@ export class HoldenWreckTheme extends Theme {
   renderHTML(person: Person): Promise<string> {
     return this.base.renderHTML(person);
   }
-
-  async renderCSS(person: Person): Promise<string> {
-    const css = await this.base.renderCSS(person);
-    return (
-      css +
-      `:root {
-        --header-color: #6b5a3a;
-        --text-color: #2f3a2e;
-        --heading-color: var(--text-color);
-        --border-color: #7a7f82;
-        --link-color: var(--text-color);
-        --link-color-visited: var(--border-color);
-        --space-1: 10px;
-        --space-2: 20px;
-        --space-3: 40px;
-        --image-size: 270px;
-        --half-header-height: calc((var(--image-size) / 2) + var(--space-3));
-      }`
-    );
-  }
 }
